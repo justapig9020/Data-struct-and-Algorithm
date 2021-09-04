b54907496c55b4a84ee2ee31e5b260aed8a8a535
justapig9020 <justapig9020@gmail.com>
2021-09-04 21:34:17 +0800

    Update graph ADT

--- graph/graph/src/lib.rs
+++ graph/graph/src/lib.rs
@@ -23,11 +23,38 @@ impl Edge {
 }
 
 #[derive(Debug)]
+struct Vertex {
+    edge: Vec<Edge>,
+    indegree: usize,
+    outdegree: usize,
+}
+
+impl Vertex {
+    fn new(v: usize, t: usize) -> Self {
+        let mut edge = vec![Edge::Discon; t];
+        edge[v] = Edge::Connect(0);
+        Self {
+            edge,
+            indegree: 0,
+            outdegree: 0,
+        }
+    }
+    fn new_edge(&mut self, adj: usize, dis: i32) {
+        self.edge[adj] = Edge::Connect(dis);
+        self.outdegree += 1;
+    }
+    fn to(&self, distination: usize) -> Option<&Edge> {
+        self.edge.get(distination)
+    }
+}
+
+#[derive(Debug)]
 pub struct Graph {
-    edge: Vec<Vec<Edge>>,
+    vertices: Vec<Vertex>,
 }
 
 impl Graph {
+    /// Generate a graph by given a name of json file
     pub fn from_config(name: &str) -> Result<Self, String> {
         let mut setting = Config::default();
         setting
@@ -46,35 +73,52 @@ impl Graph {
         Ok(Self::new(edges))
     }
     pub fn new(adj: Vec<Vec<String>>) -> Self {
-        let vertices = adj.len();
-        let mut edge = Vec::with_capacity(vertices);
-        for v in adj.iter() {
-            let mut adj = Vec::with_capacity(vertices);
-            for e in v.iter() {
-                adj.push(Edge::new(e));
+        let amount = adj.len();
+        let mut vertices = Vec::with_capacity(amount);
+        for i in 0..amount {
+            vertices.push(Vertex::new(i, amount));
+        }
+        for (i, v) in adj.iter().enumerate() {
+            for (j, e) in v.iter().enumerate() {
+                if let Ok(dis) = e.parse::<i32>() {
+                    vertices[i].new_edge(j, dis);
+                }
             }
-            edge.push(adj);
         }
         Self {
-            edge,
+            vertices,
         }
     }
+
+    /// Get distance of the edge from "a" to "b"
     pub fn distance(&self, a: usize, b: usize) -> Option<i32> {
-        let v = self.edge.get(a)?;
-        let e = v.get(b)?;
+        let v = self.vertices.get(a)?;
+        let e = v.to(b)?;
         e.to_option()
     }
+
+    /// Return edges in adjacent matrix form.
     pub fn dump_edges(&self) -> Vec<Vec<Option<i32>>> {
-        let edges = self.edge
+        let edges = self.vertices
             .iter()
-            .map(|v| v
+            .map(|v| v.edge
                 .iter()
                 .map(|e| e.to_option())
                 .collect())
             .collect();
         edges
     }
+
+    /// Return the vertices count in the graph
     pub fn amount(&self) -> usize {
-        self.edge.len()
+        self.vertices.len()
+    }
+    pub fn indegree(&self, v: usize) -> Option<usize> {
+        let v = self.vertices.get(v)?;
+        Some(v.indegree)
+    }
+    pub fn outdegree(&self, v: usize) -> Option<usize> {
+        let v = self.vertices.get(v)?;
+        Some(v.outdegree)
     }
 }
