8e9fd2b851a9c237e68404744e0bedb3f3e25796
justapig9020 <justapig9020@gmail.com>
2021-09-04 14:10:30 +0800

    Seperate insertion sort from shell sort

--- sort/shellsort/sort.c
+++ sort/shellsort/sort.c
@@ -7,20 +7,24 @@ static void swap(int *a, int *b) {
     *a ^= *b;
 }
 
+static void insertion_sort(int arr[], size_t n, size_t gap) {
+    for (int i=gap; i<n; i++) {
+        int curr = i;
+        int last = curr - gap;
+        while (0 <= last &&
+                arr[curr] < arr[last]) {
+            swap(&arr[curr], &arr[last]);
+            curr = last;
+            last = curr - gap;
+        }
+    }
+}
+
 void shell_sort(int arr[], size_t n)
 {
     int gap = n / 2;
     while (gap > 0) {
-        for (int i=gap; i<n; i++) {
-            int curr = i;
-            int last = curr - gap;
-            while (0 <= last &&
-                    arr[curr] < arr[last]) {
-                swap(&arr[curr], &arr[last]);
-                curr = last;
-                last = curr - gap;
-            }
-        }
+        insertion_sort(arr, n, gap);
         gap /= 2;
     }
 }
